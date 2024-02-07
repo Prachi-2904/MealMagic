d7b2c99a82b49ab53be7b7d596d523d6e327f45a
unknown <wasankarsakshi@gmai.com>
2024-02-07 11:26:15 +0530

    Pojos for backend done

---
+++ backend/spring_boot_backend_template/src/main/java/com/app/entities/RatingAndReviews.java
@@ -0,0 +1,19 @@
+package com.app.entities;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="rating_and_review")
+public class RatingAndReviews extends BaseEntity {
+	private int rating;
+
+    @Column(length = 500) 
+    private String review;
+}
